c95c8034a4c72fc23be18086a7e366e022e52e89
Aaron Johnson <aarontropy@gmail.com>
2017-02-20 16:35:23 -0500

    add week5 content

---
+++ week5/src/models/file.model.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const FileSchema = new mongoose.Schema({
+  filename: String,
+  title: String,
+  updated_at: { type: Date, default: Date.now },
+});
+
+const File = mongoose.model('File', FileSchema);
+
+// Seed the database if empty
+File.count({}, function(err, count) {
+  if (err) {
+    throw err;
+  }
+  const files = require('./file.seed.json');
+  File.create(files, function(err, newFiles) {
+    if (err) {
+      throw err;
+    }
+  });
+});
+
+module.exports = File
