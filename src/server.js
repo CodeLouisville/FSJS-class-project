82c89b77caf21920fcc82b2c48eb65a8caac54fe
Aaron Johnson <aarontropy@gmail.com>
2018-01-25 15:40:44 -0500

    added code from week2

--- src/server.js
+++ src/server.js
@@ -1,9 +1,13 @@
 // src/server.js
 
+const path = require('path');
 const express = require('express');
 const config = require('./config');
 
 const app = express();
+const publicPath = path.resolve(__dirname, '../public');
+app.use(express.static(publicPath));
+
 
 app.use('/doc', function(req, res, next) {
   res.end(`Documentation http://expressjs.com/`);
