97932a6070870f0b3e2acb7b2cf1e468093edee9
Aaron Johnson <aarontropy@gmail.com>
2018-06-07 13:34:15 -0400

    code set to end of week3

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,21 +5,21 @@ const router = require('express').Router();
 const FILES = [
   {id: 'a', title: 'cutecat1.jpg', description: 'A cute cat'},
   {id: 'b', title: 'uglycat1.jpg', description: 'Just kidding, all cats are cute'},
-  {id: 'c', title: 'total_recall_poster.jpg', description: 'Quaid, start the reactor...'},
-  {id: 'd', title: 'louisville_coffee.txt', description: 'Coffee shop ratings'},
+  {id: 'c', title: 'plaintext_passwords.txt', description: '12345 - like my luggage'},
+  {id: 'd', title: 'got_S08E01.mp4', description: 'home video of...a cat'},
 ];
 
-
-router.use('/doc', function(req, res, next) {
-  res.end(`Documentation http://expressjs.com/`);
+/**
+ * C - reate
+ */
+router.post('/file', function(req, res, next) {
+  res.end('Create a new file');
 });
-
-router.get('/file', function(req, res, next) {
-  res.json(FILES);
-});
-
+/**
+ * R - ead
+ */
 router.get('/file/:fileId', function(req, res, next) {
-  const {fileId} = req.params;
+  const { fileId } = req.params;
   // same as 'const fileId = req.params.fileId'
 
   const file = FILES.find(entry => entry.id === fileId);
@@ -29,19 +29,24 @@ router.get('/file/:fileId', function(req, res, next) {
 
   res.json(file);
 });
-
-router.post('/file', function(req, res, next) {
-  res.end('Create a new file');
-});
-
+/**
+ * U - pdate
+ */
 router.put('/file/:fileId', function(req, res, next) {
   res.end(`Updating file '${req.params.fileId}'`);
 });
-
+/**
+ * D - elete
+ */
 router.delete('/file/:fileId', function(req, res, next) {
   res.end(`Deleting file '${req.params.fileId}'`);
 });
-
+/**
+ * ¯\_(ツ)_/¯ - list
+ */
+router.get('/file', function(req, res, next) {
+  res.json(FILES);
+});
 
 module.exports = router;
 
--- src/server.js
+++ src/server.js
@@ -7,9 +7,9 @@ const router = require('./routes');
 
 const app = express();
 const publicPath = path.resolve(__dirname, '../public');
+
 app.use(express.static(publicPath));
 app.use('/api', router);
-
 
 app.listen(config.port, function() {
   console.log(`${config.appName} is listening on port ${config.port}`);
