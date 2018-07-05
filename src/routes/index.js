a31d38105c6d383eb4cac089e87ff1db50bfd6dd
Aaron Johnson <aarontropy@gmail.com>
2018-07-05 14:55:18 -0400

    code up to beginning of week 7

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,48 +6,13 @@ const mongoose = require('mongoose');
 const FILES = [
   {id: 'a', title: 'cutecat1.jpg', description: 'A cute cat'},
   {id: 'b', title: 'uglycat1.jpg', description: 'Just kidding, all cats are cute'},
-  {id: 'c', title: 'total_recall_poster.jpg', description: 'Quaid, start the reactor...'},
-  {id: 'd', title: 'louisville_coffee.txt', description: 'Coffee shop ratings'},
+  {id: 'c', title: 'plaintext_passwords.txt', description: '12345 - like my luggage'},
+  {id: 'd', title: 'got_S08E01.mp4', description: 'home video of...a cat'},
 ];
 
 
-router.use('/doc', function(req, res, next) {
-  res.end(`Documentation http://expressjs.com/`);
-});
-
 /**
- * Get a list of all files in the DB
- */
-router.get('/file', function(req, res, next) {
-  const fileModel = mongoose.model('File');
-
-  fileModel.find({}, function(err, files) {
-    if (err) {
-      console.log(err);
-      return res.status(500).json(err);
-    }
-  
-    res.json(files);
-  });
-});
-
-/**
- * Get a single file by passing its id as a URL param
- */
-router.get('/file/:fileId', function(req, res, next) {
-  const {fileId} = req.params;
-  // same as 'const fileId = req.params.fileId'
-
-  const file = FILES.find(entry => entry.id === fileId);
-  if (!file) {
-    return res.status(404).end(`Could not find file '${fileId}'`);
-  }
-
-  res.json(file);
-});
-
-/**
- * Create a new file
+ * C - reate
  */
 router.post('/file', function(req, res, next) {
   const File = mongoose.model('File');
@@ -58,7 +23,7 @@ router.post('/file', function(req, res, next) {
 
   File.create(fileData, function(err, newFile) {
     if (err) {
-      console.log(err);
+      console.error(err);
       return res.status(500).json(err);
     }
 
@@ -66,44 +31,50 @@ router.post('/file', function(req, res, next) {
   });
 });
 
+
 /**
- * Update an existing file
+ * R - ead
+ */
+router.get('/file/:fileId', function(req, res, next) {
+  const { fileId } = req.params;
+  // same as 'const fileId = req.params.fileId'
+
+  const file = FILES.find(entry => entry.id === fileId);
+  if (!file) {
+    return res.status(404).end(`Could not find file '${fileId}'`);
+  }
+
+  res.json(file);
+});
+/**
+ * U - pdate
  */
 router.put('/file/:fileId', function(req, res, next) {
-  const File = mongoose.model('File');
-  const fileId = req.params.fileId;
+  const data = req.body;
+  console.log("PUT DATA", data);
 
-  File.findById(fileId, function(err, file) {
-    if (err) {
-      console.error(err);
-      return res.status(500).json(err);
-    }
-    if (!file) {
-      return res.status(404).json({message: "File not found"});
-    }
-
-    file.title = req.body.title;
-    file.description = req.body.description;
-
-    file.save(function(err, savedFile) {
-      if (err) {
-        console.error(err);
-        return res.status(500).json(err);
-      }
-      res.json(savedFile);
-    })
-
-  })
-
+  res.end(`Updating file '${req.params.fileId}'`);
 });
 
 /**
- * Delete a file
+ * D - elete
  */
 router.delete('/file/:fileId', function(req, res, next) {
   res.end(`Deleting file '${req.params.fileId}'`);
 });
-
+/**
+ * ¯\_(ツ)_/¯ - list
+ */
+router.get('/file', function(req, res, next) {
+  mongoose.model('File').find({}, function(err, files) {
+    if (err) {
+      console.log(err);
+      return res.status(500).json(err);
+    }
+  
+    res.json(files);
+  });
+  
+});
 
 module.exports = router;
-
